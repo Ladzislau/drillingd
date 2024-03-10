18009b286090fdc41dbd45446394a312a05240b8
vlad <mainvgd@gmail.com>
2024-03-11 01:29:55 +0300

    implement controller

--- src/main/java/by/upmebel/upmecutfile/web/controller/ElementController.java
+++ src/main/java/by/upmebel/upmecutfile/web/controller/ElementController.java
@@ -5,14 +5,13 @@ import by.upmebel.upmecutfile.service.ElementSideService;
 import by.upmebel.upmecutfile.service.HoleService;
 import by.upmebel.upmecutfile.web.dto.request.element.CreateElementRequest;
 import by.upmebel.upmecutfile.web.dto.request.element.UpdateElementRequest;
-import by.upmebel.upmecutfile.web.dto.request.hole.CreateHolesRequest;
+import by.upmebel.upmecutfile.web.dto.request.hole.CreateHoleRequest;
 import by.upmebel.upmecutfile.web.dto.request.hole.UpdateHoleRequest;
-import by.upmebel.upmecutfile.web.dto.response.element.CreateElementResponse;
-import by.upmebel.upmecutfile.web.dto.response.element.GetElementResponse;
-import by.upmebel.upmecutfile.web.dto.response.element.GetElementsPageResponse;
-import by.upmebel.upmecutfile.web.dto.response.element.UpdateElementResponse;
-import by.upmebel.upmecutfile.web.dto.response.hole.CreateHolesResponse;
-import by.upmebel.upmecutfile.web.dto.response.hole.UpdateHoleResponse;
+import by.upmebel.upmecutfile.web.dto.response.element.ElementInfo;
+import by.upmebel.upmecutfile.web.dto.response.element.ElementsPageResponse;
+import by.upmebel.upmecutfile.web.dto.response.hole.HoleInfo;
+import by.upmebel.upmecutfile.web.dto.response.side.ElementSideInfo;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -30,63 +29,75 @@ public class ElementController {
     private final HoleService holeService;
 
     @GetMapping
-    public ResponseEntity<GetElementsPageResponse> getElements(
-            @RequestParam(value = "page", defaultValue = "0") int pageNumber,
-            @RequestParam(value = "size", defaultValue = "10") int pageSize) {
+    public ResponseEntity<ElementsPageResponse> getElements(
+            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
-//        GetElementsPageResponse response = elementService.getElements(pageable);
-        return ResponseEntity.ok(null);
+        ElementsPageResponse response = elementService.getElements(pageable);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<CreateElementResponse> createElement(@RequestBody CreateElementRequest request) {
-        CreateElementResponse response = elementService.createElement(request);
-        return ResponseEntity.ok(null);
+    public ResponseEntity<ElementInfo> createElement(@RequestBody @Valid CreateElementRequest request) {
+        ElementInfo response = elementService.createElement(request);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{elementId}")
-    public ResponseEntity<GetElementResponse> getElement(@PathVariable("elementId") Long elementId) {
-//        GetElementResponse response = elementService.getElementById(elementId);
-        return ResponseEntity.ok(null);
+    public ResponseEntity<ElementInfo> getElement(@PathVariable("elementId") Long elementId) {
+        ElementInfo response = elementService.getElementById(elementId);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{elementId}")
-    public ResponseEntity<UpdateElementResponse> updateElement(@PathVariable("elementId") Long elementId,
-                                                               @RequestBody UpdateElementRequest request) {
-//        UpdateElementResponse response = elementService.updateElement(elementId, request);
-        return ResponseEntity.ok(null);
+    public ResponseEntity<ElementInfo> updateElement(@PathVariable("elementId") Long elementId,
+                                                     @RequestBody @Valid UpdateElementRequest request) {
+        ElementInfo response = elementService.updateElement(elementId, request);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{elementId}")
     public void deleteElement(@PathVariable("elementId") Long elementId) {
+        elementService.deleteById(elementId);
+    }
 
-//        elementService.deleteById(elementId);
+    @GetMapping("/{elementId}/sides/{sideId}")
+    public ResponseEntity<ElementSideInfo> getSide(@PathVariable("elementId") Long elementId,
+                                                   @PathVariable("sideId") Long sideId){
+        ElementSideInfo response = elementSideService.getSideByIds(elementId, sideId);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/{elementId}/sides/{sideId}/holes")
-    public ResponseEntity<CreateHolesResponse> addHoles(@PathVariable("elementId") Long elementId,
-                                                        @PathVariable("sideId") Long sideId,
-                                                        @RequestBody CreateHolesRequest request) {
-//        CreateHolesResponse response = holeService.createHoles(elementId, sideId, request);
-        return ResponseEntity.ok(null);
+    public ResponseEntity<HoleInfo> addHole(@PathVariable("elementId") Long elementId,
+                                            @PathVariable("sideId") Long sideId,
+                                            @RequestBody @Valid CreateHoleRequest request) {
+        HoleInfo response = holeService.createHole(elementId, sideId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{elementId}/sides/{sideId}/holes/{holeId}")
+    public ResponseEntity<HoleInfo> getHole(@PathVariable("elementId") Long elementId,
+                                               @PathVariable("sideId") Long sideId,
+                                               @PathVariable("holeId") Long holeId) {
+        HoleInfo response = holeService.getHoleByIds(elementId, sideId, holeId);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{elementId}/sides/{sideId}/holes/{holeId}")
-    public ResponseEntity<UpdateHoleResponse> updateHole(@PathVariable("elementId") Long elementId,
-                                                         @PathVariable("sideId") Long sideId,
-                                                         @PathVariable("holeId") Long holeId,
-                                                         @RequestBody UpdateHoleRequest request) {
-//        чекнуть айдишники на валидность
-//        CreateHolesResponse response = holeService.updateHole(elementId, sideId, holeId, request);
-        return ResponseEntity.ok(null);
+    public ResponseEntity<HoleInfo> updateHole(@PathVariable("elementId") Long elementId,
+                                               @PathVariable("sideId") Long sideId,
+                                               @PathVariable("holeId") Long holeId,
+                                               @RequestBody @Valid UpdateHoleRequest request) {
+        HoleInfo response = holeService.updateHole(elementId, sideId, holeId, request);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{elementId}/sides/{sideId}/holes/{holeId}")
     public void deleteHole(@PathVariable("elementId") Long elementId,
-                           @PathVariable("sideId") String sideId,
+                           @PathVariable("sideId") Long sideId,
                            @PathVariable("holeId") Long holeId) {
-
-//        holeService.deleteById(elementId, sideId, holeId);
+        holeService.deleteById(elementId, sideId, holeId);
     }
 
 }
