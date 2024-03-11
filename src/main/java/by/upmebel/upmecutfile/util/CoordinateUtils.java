b5035d5690bf59441028d3c25859340b8010269e
vlad <mainvgd@gmail.com>
2024-03-11 13:35:57 +0300

    remove redundant field and method

--- src/main/java/by/upmebel/upmecutfile/util/CoordinateUtils.java
+++ src/main/java/by/upmebel/upmecutfile/util/CoordinateUtils.java
@@ -8,15 +8,11 @@ import java.util.regex.Pattern;
 
 public class CoordinateUtils {
 
-    public static final String COORDINATE_REGEX = "(\\s*([LlBbHh]|(\\d+(\\.\\d+)?))(?!\\w|\\d)\\s*)(\\s*[+\\-*/]\\s*([LlBbHh]|(\\d+(\\.\\d+)?))(?!\\w|\\d)\\s*)*";
     public static final String NUMBER_REGEX = "\\s*\\d+(\\.\\d+)?\\s*$";
     private static final String LENGTH_ABBREVIATION = ElementSideParameter.LENGTH.getValue();
     private static final String BREADTH_ABBREVIATION = ElementSideParameter.BREADTH.getValue();
     private static final String HEIGHT_ABBREVIATION = ElementSideParameter.HEIGHT.getValue();
 
-    public static boolean isCoordinateValid(String coordinate) {
-        return Pattern.matches(COORDINATE_REGEX, coordinate);
-    }
     public static boolean isCoordinateNumeric(String coordinate) {
         return Pattern.matches(NUMBER_REGEX, coordinate);
     }
